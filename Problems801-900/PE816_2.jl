3c0eb2c68489eed0ab176ac53aac63c7d521de07
Alex <51973026+elvispy@users.noreply.github.com>
2024-11-26 21:13:18 -0500

    Solved problem PE816 using space partitioning and binned searching

--- Problems801-900/PE816_2.jl
+++ Problems801-900/PE816_2.jl
@@ -1,94 +1,101 @@
-# PE816
 
 """
-The idea is to subdivide the plane intro small grids 
+PE 816.jl
+The idea is to subdivide the plane into small regions and perform nearest search on those small regions
+This way, we only need to test for proximity in adjacent bins.
+
+Since the maximum value possible for the coordinate of the points is approximately 5e+7, which has
+26 digits in binary, the bin is going to be the first 10 digits in binary of the two coodinates,
+being thus a tuple of two 10-digit binary numbers. 
+
+See
+https://en.m.wikipedia.org/wiki/Nearest_neighbor_search#Space_partitioning
+For the idea of this algorithm
+
 """
-# using StatsBase;
-using DataStructures
-
-N = 14; # Number of points
-M = 50515093; # mod
-G = 2;
-Points = [290797.0, mod(290797.0^2, M)];
-
-struct points
-    x::Int64
-    y::Int64
+# Function to convert a number to its first 5 binary digits
+function first_5_binary_digits(x)
+    return div(x, 2^15)
 end
 
-PointsGrid = Matrix{Vector{points}(undef, N)}(undef, N, G, G);
-
-for ii = 1:N
-
-end
-
-
-println("Calculated Points!");
-
-
-G = 10; # Number of lines to project to
-angs = LinRange(0, pi-pi/100, G);
-angsX = cos.(angs);
-angsY = sin.(angs);
-
-min_distance = Inf;
-Distance(p1, p2) = sqrt((p1[1] - p2[1])^2 + (p1[2] - p2[2])^2)
-for ii = 1:G
-    global min_distance
-    f(p) = p[1] * angsX[ii] + p[2] * angsY[ii];
-
-    sort!(Points, by = f); 
-
-    A = Points;
-    for jj = 1:N
-        # Checking items less that the point
-        if mod(jj, 100000) == 0; println("Im in $jj, $ii"); end
-        ll = jj - 1;
-        while ll >= 1 && abs(f(A[ll]) - f(A[jj])) < min_distance
-            if Distance(A[ll], A[jj]) < min_distance
-                min_distance = Distance(A[ll], A[jj]);
-            end
-            ll = ll - 1;
+# Function to bin points based on their first 5 binary digits
+function bin_points(points)
+    bins = Dict{Tuple{Int, Int}, Vector{Tuple{Float64, Float64}}}()
+    for point in points
+        x_bin = first_5_binary_digits(point[1])
+        y_bin = first_5_binary_digits(point[2])
+        bin_key = (x_bin, y_bin)
+        if !haskey(bins, bin_key)
+            bins[bin_key] = []
         end
-
-        ll = jj + 1;
-        while ll <= N && abs(f(A[ll]) - f(A[jj])) < min_distance
-            if Distance(A[ll], A[jj]) < min_distance
-                min_distance = Distance(A[ll], A[jj]);
-            end
-            ll = ll + 1;
-        end
-
+        push!(bins[bin_key], point)
     end
+    return bins
 end
 
-function slow_checker(Points)
-    L = length(Points);
-    min_distance = Inf;
-    for jj = 1:L
-        for ii = (jj+1):L
-            if Distance(Points[ii], Points[jj]) < min_distance
-                min_distance = Distance(Points[ii], Points[jj]); 
+# Function to calculate Euclidean distance between two points
+function euclidean_distance(p1, p2)
+    return sqrt((p1[1] - p2[1])^2 + (p1[2] - p2[2])^2)
+end
+
+# Function to find the minimum distance in a list of points
+function min_distance_in_list(points)
+    min_dist = Inf
+    for i in 1:length(points)-1
+        for j in i+1:length(points)
+            dist = euclidean_distance(points[i], points[j])
+            if dist < min_dist
+                min_dist = dist
             end
         end
     end
-    return min_distance;
+    return min_dist
 end
 
-
-function calculate_min(Points)
-    SIDES = 200;
-    L = M / SIDES; # Divide the full grid on smaller SIDES x SIDES 
-    X = [Vector{Float64}[] for _ = 1:SIDES];
-    GRID = [X for _ = 1:SIDES];
-    for ii = 1:N
-        x, y = ceil.(Int64, Points[ii]/L);
-        push!(GRID[x][y], Points[ii]);
+# Function to find the closest distance between points in adjacent bins
+function min_distance_between_bins(bin1, bin2)
+    min_dist = Inf
+    for p1 in bin1
+        for p2 in bin2
+            dist = euclidean_distance(p1, p2)
+            if dist < min_dist
+                min_dist = dist
+            end
+        end
     end
+    return min_dist
 end
 
-
-# println("The slow result is: ", slow_checker(Points));
-println("The fast result is: ", min_distance); # Prints 20.880613017
-
-
+# Main function to find the closest Euclidean distance between any pair of points
+function closest_distance(points)
+    bins = bin_points(points)
+    min_dist = Inf
+    for ((x_bin, y_bin), bin_points) in bins
+        # Check distance within the same bin
+        min_dist = min(min_dist, min_distance_in_list(bin_points))
+        # Check distances with adjacent bins
+        for dx in -1:1
+            for dy in -1:1
+                if dx != 0 || dy != 0
+                    adjacent_bin_key = (x_bin + dx, y_bin + dy)
+                    if haskey(bins, adjacent_bin_key)
+                        min_dist = min(min_dist, min_distance_between_bins(bin_points, bins[adjacent_bin_key]))
+                    end
+                end
+            end
+        end
+    end
+    return min_dist
+end
+N = 2000000
+s0 = 290797.0;
+M = 50515093.0
+points = Vector{Tuple{Float64, Float64}}()
+for i = 1:N
+    global s0;
+    s1 = convert(Float64, mod(s0^2, M));
+    push!(points, (s0, s1));
+    s0 = convert(Float64, mod(s1^2, M));
+end
+# Example usage
+println("Closest distance: ", closest_distance(points))
